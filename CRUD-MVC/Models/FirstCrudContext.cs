4858a65edc47ead82424abb23ac13ddae47da675
KDOE-117 <jdn2037@gmail.com>
2023-04-26 18:19:15 -0500

    Agregar archivos de proyecto.

---
+++ CRUD-MVC/Models/FirstCrudContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_MVC.Models;
+
+public partial class FirstCrudContext : DbContext
+{
+    public FirstCrudContext()
+    {
+    }
+
+    public FirstCrudContext(DbContextOptions<FirstCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+        }
+    }
+        
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        //=> optionsBuilder.UseSqlServer("");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.ToTable("Persona");
+
+            entity.Property(e => e.Id).HasColumnName("id")
+                .ValueGeneratedOnAdd();
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
